cf1f5dacc81916b89a5d11948715a92b4758207c
dlotts@knightrider.com <dlotts@knightrider.com>
2022-04-07 17:37:02 -0700

    refactor: usee anon function with ClientOnly

--- app/routes/r3f.tsx
+++ app/routes/r3f.tsx
@@ -5,7 +5,7 @@ import CanvasWrapperClient from '~/pano/canvas-wrapper.client';
 export default function R3fPage() {
   return (
     <main className="flex h-full bg-white">
-      <ClientOnly fallback={null} children={CanvasWrapperClient} />
+        <ClientOnly fallback={null} children={() => <CanvasWrapperClient/>} />
     </main>
   );
 }
